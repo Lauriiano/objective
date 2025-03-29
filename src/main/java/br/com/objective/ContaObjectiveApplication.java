6bf2ffd008b57ec820b2081d10fb4a4e86eee924
Acran Laureano <acran.araujo@gmail.com>
2025-03-29 16:03:44 -0300

    commit inicial, estrutura basica de pastas.

---
+++ src/main/java/br/com/objective/ContaObjectiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.objective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaObjectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaObjectiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/objective/ContaObjectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.objective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaObjectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
