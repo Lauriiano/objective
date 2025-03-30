6b306a93ba253666661c60483a51aae09a0d0e5a
Acran Laureano <acran.araujo@gmail.com>
2025-03-30 17:59:48 -0300

    criado controllers para expor os recursos.

--- src/main/java/br/com/objective/controller/ContaController.java
+++ src/main/java/br/com/objective/controller/ContaController.java
@@ -1,5 +1,42 @@
 package br.com.objective.controller;
 
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.objective.dto.ContaDto;
+import br.com.objective.model.Conta;
+import br.com.objective.service.ContaService;
+
+@RestController
+@RequestMapping("/conta")
 public class ContaController {
 
+	@Autowired
+	ContaService contaService;
+	
+	@PostMapping
+	public ResponseEntity<Conta> create(@RequestBody ContaDto dto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(contaService.create(dto));
+	}
+	
+	@GetMapping("{numero_conta}")
+	public ResponseEntity<Conta> findByInd(@PathVariable("numero_conta") Integer numero_conta) {
+		return ResponseEntity.status(HttpStatus.OK).body(contaService.findById(numero_conta));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Conta>> findAll() {
+		return ResponseEntity.status(HttpStatus.OK).body(contaService.findAll());
+	}
+	
 }
--- src/main/java/br/com/objective/controller/TransacaoController.java
+++ src/main/java/br/com/objective/controller/TransacaoController.java
@@ -1,5 +1,27 @@
 package br.com.objective.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.objective.dto.TransacaoDto;
+import br.com.objective.dto.TransacaoResponseDto;
+import br.com.objective.service.TransacaoService;
+
+@RestController
+@RequestMapping("/transacao")
 public class TransacaoController {
 
+	@Autowired
+	TransacaoService transacaoService;
+	
+	@PostMapping
+	public ResponseEntity<TransacaoResponseDto> transacao(@RequestBody TransacaoDto dto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(transacaoService.transacao(dto));
+	}
+	
 }
